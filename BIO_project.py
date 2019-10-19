f1b087e0954e331b684e5a77819703cb06047a44
Dávid Bolvanský <david.bolvansky@gmail.com>
2019-10-19 18:08:45 +0200

    add options to script

--- BIO_project.py
+++ BIO_project.py
@@ -1,16 +1,16 @@
-from skimage import img_as_ubyte
 import pandas as pd
 import numpy as np
 import glob
 import os
 import sys
+import argparse
 import pathlib
 import shutil
 import face_recognition
 import matplotlib
 import matplotlib.pyplot as plt
 
-from skimage import io
+from skimage import io, img_as_ubyte
 from skimage.transform import resize
 from PIL import Image, ImageEnhance
 from Detectors import Detectors
@@ -18,8 +18,9 @@ from Detectors import Detectors
 brightness_factors = [0.1, 0.3, 0.6, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
 downscale_factors = [1.0, 0.8, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
 
+
 def filter_images_by_face_width(img_dir, face_width, img_limit, out_dir):
-    images = list(pathlib.Path(img_dir).rglob("*.jpg"))
+    images = list(pathlib.Path(img_dir).rglob("*.*"))
 
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -35,10 +36,11 @@ def filter_images_by_face_width(img_dir, face_width, img_limit, out_dir):
                 shutil.copy(img, out_dir)
                 c += 1
                 if c == img_limit:
-                    return
+                    return img_limit
+    return c
 
 
-def do_analysis_brightness(base_dir):
+def do_analysis_brightness_effect(base_dir, brightness_factors):
     inner_dirs = [x[0] for x in os.walk(base_dir) if x[0] is not base_dir]
     r1 = []
     r2 = []
@@ -54,7 +56,6 @@ def do_analysis_brightness(base_dir):
             det5 = 0
             all_files = len(os.listdir(dir))
             for file in os.listdir(dir):
-                #print("File: " , file)
                 D = Detectors(os.path.join(dir, file))
                 if D.detectFaceViaFaceRecognition():
                     det1 += 1
@@ -72,23 +73,17 @@ def do_analysis_brightness(base_dir):
             r3.append((det3 * 100.0) / all_files)
             r4.append((det4 * 100.0) / all_files)
             r5.append((det5 * 100.0) / all_files)
-            print("Rasa: ", os.path.basename(dir))
-            print("Face recognition: ", det1)
-            print("Haar: ", det2)
-            print("HoG: ", det3)
-            print("CNN: ", det4)
-            print("DNN: ", det5)
 
-    #df = pd.DataFrame({'x': np.array(brightness_factors), 'd1': np.array(r1), 'd2': np.array(
-    #    r2), 'd3': np.array(r3), 'd4': np.array(r4), 'd5': np.array(r5)})
-    #df['x'] = df['x'].astype(str)
-
-
-    plt.bar(np.array(brightness_factors) - 0.08, r1, color = 'blue', width = 0.03, label="Face recognition")
-    plt.bar(np.array(brightness_factors) - 0.04, r2, color = 'green', width = 0.03, label="Haar (opencv)")
-    plt.bar(np.array(brightness_factors) , r3, color = 'red', width = 0.03, label="HoG (dlib)")
-    plt.bar(np.array(brightness_factors) + 0.04, r4, color = 'black', width = 0.03, label="CNN (dlib)")
-    plt.bar(np.array(brightness_factors) + 0.08, r5, color = 'yellow', width = 0.03, label="DNN (cvlib)")
+    plt.bar(np.array(brightness_factors) - 0.08, r1,
+            color='blue', width=0.03, label="Face recognition")
+    plt.bar(np.array(brightness_factors) - 0.04, r2,
+            color='green', width=0.03, label="Haar (opencv)")
+    plt.bar(np.array(brightness_factors), r3,
+            color='red', width=0.03, label="HoG (dlib)")
+    plt.bar(np.array(brightness_factors) + 0.04, r4,
+            color='black', width=0.03, label="CNN (dlib)")
+    plt.bar(np.array(brightness_factors) + 0.08, r5,
+            color='yellow', width=0.03, label="DNN (cvlib)")
     plt.xticks(np.array(brightness_factors))
     plt.ylabel('úspešnosť detekcie (%)')
     plt.xlabel('faktor jasu')
@@ -96,7 +91,8 @@ def do_analysis_brightness(base_dir):
     plt.legend()
     plt.show()
 
-def do_analysis_scale(base_dir):
+
+def do_analysis_downscale_effect(base_dir, downscale_factors):
     inner_dirs = [x[0] for x in os.walk(base_dir) if x[0] is not base_dir]
     r1 = []
     r2 = []
@@ -112,7 +108,6 @@ def do_analysis_scale(base_dir):
             det5 = 0
             all_files = len(os.listdir(dir))
             for file in os.listdir(dir):
-                #print("File: " , file)
                 D = Detectors(os.path.join(dir, file))
                 if D.detectFaceViaFaceRecognition():
                     det1 += 1
@@ -120,10 +115,8 @@ def do_analysis_scale(base_dir):
                     det2 += 1
                 if D.detectFaceViaHoGFaceDetector():
                     det3 += 1
-                print(file)
                 if D.detectFaceViaCNNFaceDetector():
                     det4 += 1
-                print(file)
                 if D.detectFaceViaCVLIBFaceDetector():
                     det5 += 1
 
@@ -132,23 +125,17 @@ def do_analysis_scale(base_dir):
             r3.append((det3 * 100.0) / all_files)
             r4.append((det4 * 100.0) / all_files)
             r5.append((det5 * 100.0) / all_files)
-            print("Rasa: ", os.path.basename(dir))
-            print("Face recognition: ", det1)
-            print("Haar: ", det2)
-            print("HoG: ", det3)
-            print("CNN: ", det4)
-            print("DNN: ", det5)
 
-    #df = pd.DataFrame({'x': np.array(brightness_factors), 'd1': np.array(r1), 'd2': np.array(
-    #    r2), 'd3': np.array(r3), 'd4': np.array(r4), 'd5': np.array(r5)})
-    #df['x'] = df['x'].astype(str)
-
-
-    plt.bar(np.array(downscale_factors) - 0.025, r1, color = 'blue', width = 0.015, label="Face recognition")
-    plt.bar(np.array(downscale_factors) - 0.01, r2, color = 'green', width = 0.015, label="Haar (opencv)")
-    plt.bar(np.array(downscale_factors) , r3, color = 'red', width = 0.015, label="HoG (dlib)")
-    plt.bar(np.array(downscale_factors) + 0.01, r4, color = 'black', width = 0.015, label="CNN (dlib)")
-    plt.bar(np.array(downscale_factors) + 0.025, r5, color = 'yellow', width = 0.015, label="DNN (cvlib)")
+    plt.bar(np.array(downscale_factors) - 0.025, r1,
+            color='blue', width=0.015, label="Face recognition")
+    plt.bar(np.array(downscale_factors) - 0.01, r2,
+            color='green', width=0.015, label="Haar (opencv)")
+    plt.bar(np.array(downscale_factors), r3,
+            color='red', width=0.015, label="HoG (dlib)")
+    plt.bar(np.array(downscale_factors) + 0.01, r4,
+            color='black', width=0.015, label="CNN (dlib)")
+    plt.bar(np.array(downscale_factors) + 0.025, r5,
+            color='yellow', width=0.015, label="DNN (cvlib)")
     plt.xticks(np.array(downscale_factors))
     plt.ylabel('úspešnosť detekcie (%)')
     plt.xlabel('downscale faktor')
@@ -156,17 +143,10 @@ def do_analysis_scale(base_dir):
     plt.legend()
     plt.show()
 
-#do_analysis_all("/home/xbolva00/face_detect/BIO_face_detectors/Dataset/tt/")
-#filter_images_by_face_width("/home/xbolva00/face_detect/2003/01/19/big/", 90, 110, "./ou2t")
 
-
-def downscale_images_in_folder(img_dir, downscale_factor):
-    if downscale_factor > 1.0:
-        print("Error: downscale factor must be < 1")
-        return
-
-    images = list(pathlib.Path(img_dir).rglob("*.jpg"))
-    out_dir = os.path.join(pathlib.Path(img_dir).parents[0], os.path.dirname(img_dir) + "_downscale_" + str(downscale_factor))
+def downscale_images(img_dir, downscale_factor, base_out_dir):
+    images = list(pathlib.Path(img_dir).rglob("*.*"))
+    out_dir = os.path.join(base_out_dir, "downscale_" + str(downscale_factor))
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
@@ -174,29 +154,78 @@ def downscale_images_in_folder(img_dir, downscale_factor):
         image = io.imread(img)
         new_x = (int)(image.shape[0] * downscale_factor)
         new_y = (int)(image.shape[1] * downscale_factor)
-        image_downscaled = resize(image, (new_x, new_y),
-                                  anti_aliasing=True)
+        image_downscaled = resize(image, (new_x, new_y), anti_aliasing=True)
         io.imsave(os.path.join(out_dir, os.path.basename(img)),
                   img_as_ubyte(image_downscaled))
 
-def change_brightness_for_images_in_folder(img_dir, brightness_factor):
-    images = list(pathlib.Path(img_dir).rglob("*.jpg"))
-    out_dir = os.path.join(pathlib.Path(img_dir).parents[0], os.path.dirname(img_dir) + "_brightness_" + str(brightness_factor))
+
+def change_brightness_of_images(img_dir, brightness_factor, base_out_dir):
+    images = list(pathlib.Path(img_dir).rglob("*.*"))
+    out_dir = os.path.join(
+        base_out_dir, "brightness_" + str(brightness_factor))
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     for img in images:
         im = Image.open(img)
         enhancer = ImageEnhance.Brightness(im)
-        newimg = enhancer.enhance(brightness_factor) # set FACTOR > 1 to enhance contrast, < 1 to decrease
+        newimg = enhancer.enhance(brightness_factor)
         newimg.save(os.path.join(out_dir, os.path.basename(img)))
 
-def change_brightness(strain):
-    for i in brightness_factors:
-        change_brightness_for_images_in_folder("/home/xbolva00/BIO_face_detectors/Dataset/rozslisenie/" + strain + "/", i)
 
-def change_scale(strain):
-    for i in downscale_factors:
-        downscale_images_in_folder("/home/xbolva00/BIO_face_detectors/Dataset/rozslisenie/" + strain + "/", i)
+def main():
+    global brightness_factors
+    global downscale_factors
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dir", help="working directory",
+                        default=os.getcwd(), nargs='?')
+    parser.add_argument("--filter-face-width",
+                        help="filter faces by certain face width, copy them to output dir", type=int, const=90, nargs='?')
+    parser.add_argument("--filter-face-limit",
+                        help="maximal number of faces filtered to output dir", type=int, default=100, nargs='?')
+    parser.add_argument("--output-dir", help="working directory",
+                        default=os.getcwd() + "/out", nargs='?')
+    parser.add_argument("--change-brightness",
+                        help="change brightness factor of images; factor > 1 to enhance brightness, factor < 1 to decrease", type=float)
+    parser.add_argument("--change-brightness-default-factors",
+                        help="change brightness of images with default factors: " + str(brightness_factors), type=float)
+    parser.add_argument(
+        "--downscale", help="downscale images by downscale factor; factor must be in range (0, 1>", type=float)
+    parser.add_argument("--downscale-default-factors",
+                        help="downscale with default factors: " + str(downscale_factors), type=float)
+    parser.add_argument("--brightness-effect-analysis-default-factors",
+                        help="try various face detection tools on lightened/dimmed face images and show results as a graph")
+    parser.add_argument("--downscale-effect-analysis-default-factors",
+                        help="try various face detection tools on downscalled face images and show results as a graph")
+    args = parser.parse_args()
+    print(args)
+    if args.filter_face_width:
+        count = filter_images_by_face_width(
+            args.input_dir, args.filter_face_width, args.filter_face_limit, args.output_dir)
+        print("Found %d images. Images were saved to: %s" %
+              (count, args.output_dir))
+    elif args.change_brightness:
+        change_brightness_of_images(
+            args.input_dir, args.change_brightness, args.output_dir)
+        print("Images were saved to:", args.output_dir)
+    elif args.downscale:
+        if args.downscale > 1.0:
+            print("Error: downscale factor must be < 1", file=sys.stderr)
+        else:
+            downscale_images(args.input_dir, args.downscale, args.output_dir)
+            print("Images were saved to:", args.output_dir)
+    elif args.change_brightness_default_factors:
+        for f in brightness_factors:
+            change_brightness_of_images(args.input_dir, f, args.output_dir)
+        print("Images were saved to:", args.output_dir)
+    elif args.downscale_default_factors:
+        for f in downscale_factors:
+            downscale_images(args.input_dir, f, args.output_dir)
+        print("Images were saved to:", args.output_dir)
+    elif args.brightness_effect_analysis_default_factors:
+        do_analysis_brightness_effect(args.input_dir, brightness_factors)
+    elif args.downscale_effect_analysis_default_factors:
+        do_analysis_downscale_effect(args.input_dir, downscale_factors)
 
-#change_brightness("fake")
-do_analysis_brightness("/home/xbolva00/BIO_face_detectors/Dataset/rozslisenie/res/")
+
+if __name__ == "__main__":
+    main()
