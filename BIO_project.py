30a4f47c672c14fd72a1339cc28dae0b60e7f45c
Dávid Bolvanský <david.bolvansky@gmail.com>
2019-10-25 23:50:55 +0200

    report almost done

--- BIO_project.py
+++ BIO_project.py
@@ -15,9 +15,6 @@ from skimage.transform import resize
 from PIL import Image, ImageEnhance
 from Detectors import FaceDetectors
 
-brightness_factors = [0.1, 0.3, 0.6, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-downscale_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0]
-
 
 def get_images_in_folder_recursively(img_dir):
     return list(pathlib.Path(img_dir).rglob("*.jpg")) + \
@@ -66,36 +63,41 @@ def do_analysis_brightness_effect(base_dir, brightness_factors):
                 det1_tn += ret[1]
                 det1_fp += ret[2]
                 det1_fn += ret[3]
-                
+
                 ret = D.detectFaceViaHaarCascadeFaceDetector()
                 det2_tp += ret[0]
                 det2_tn += ret[1]
                 det2_fp += ret[2]
                 det2_fn += ret[3]
-                
+
                 ret = D.detectFaceViaHoGFaceDetector()
                 det3_tp += ret[0]
                 det3_tn += ret[1]
                 det3_fp += ret[2]
                 det3_fn += ret[3]
-                
+
                 ret = D.detectFaceViaCNNFaceDetector()
                 det4_tp += ret[0]
                 det4_tn += ret[1]
                 det4_fp += ret[2]
                 det4_fn += ret[3]
-                
+
                 ret = D.detectFaceViaDNNFaceDetector()
                 det5_tp += ret[0]
                 det5_tn += ret[1]
                 det5_fp += ret[2]
                 det5_fn += ret[3]
 
-            r1.append((det1_tp + det1_tn) / (det1_tp + det1_tn + det1_fp + det1_fn))
-            r2.append((det2_tp + det2_tn) / (det2_tp + det2_tn + det2_fp + det2_fn))
-            r3.append((det3_tp + det3_tn) / (det3_tp + det3_tn + det3_fp + det3_fn))
-            r4.append((det4_tp + det4_tn) / (det4_tp + det4_tn + det4_fp + det4_fn))
-            r5.append((det5_tp + det5_tn) / (det5_tp + det5_tn + det5_fp + det5_fn))
+            r1.append((det1_tp + det1_tn) /
+                      (det1_tp + det1_tn + det1_fp + det1_fn))
+            r2.append((det2_tp + det2_tn) /
+                      (det2_tp + det2_tn + det2_fp + det2_fn))
+            r3.append((det3_tp + det3_tn) /
+                      (det3_tp + det3_tn + det3_fp + det3_fn))
+            r4.append((det4_tp + det4_tn) /
+                      (det4_tp + det4_tn + det4_fp + det4_fn))
+            r5.append((det5_tp + det5_tn) /
+                      (det5_tp + det5_tn + det5_fp + det5_fn))
 
     plt.bar(np.array(brightness_factors) - 0.08, r1,
             color='blue', width=0.03, label="MTCNN")
@@ -113,6 +115,7 @@ def do_analysis_brightness_effect(base_dir, brightness_factors):
     plt.title('Vplyv zmeny jasu na presnosť detekcie tváre')
     plt.legend()
     plt.show()
+
 
 def do_analysis_downscale_effect(base_dir, downscale_factors):
     inner_dirs = [x[0] for x in os.walk(base_dir) if x[0] is not base_dir]
@@ -136,36 +139,41 @@ def do_analysis_downscale_effect(base_dir, downscale_factors):
                 det1_tn += ret[1]
                 det1_fp += ret[2]
                 det1_fn += ret[3]
-                
+
                 ret = D.detectFaceViaHaarCascadeFaceDetector()
                 det2_tp += ret[0]
                 det2_tn += ret[1]
                 det2_fp += ret[2]
                 det2_fn += ret[3]
-                
+
                 ret = D.detectFaceViaHoGFaceDetector()
                 det3_tp += ret[0]
                 det3_tn += ret[1]
                 det3_fp += ret[2]
                 det3_fn += ret[3]
-                
+
                 ret = D.detectFaceViaCNNFaceDetector()
                 det4_tp += ret[0]
                 det4_tn += ret[1]
                 det4_fp += ret[2]
                 det4_fn += ret[3]
-                
+
                 ret = D.detectFaceViaDNNFaceDetector()
                 det5_tp += ret[0]
                 det5_tn += ret[1]
                 det5_fp += ret[2]
                 det5_fn += ret[3]
 
-            r1.append((det1_tp + det1_tn) / (det1_tp + det1_tn + det1_fp + det1_fn))
-            r2.append((det2_tp + det2_tn) / (det2_tp + det2_tn + det2_fp + det2_fn))
-            r3.append((det3_tp + det3_tn) / (det3_tp + det3_tn + det3_fp + det3_fn))
-            r4.append((det4_tp + det4_tn) / (det4_tp + det4_tn + det4_fp + det4_fn))
-            r5.append((det5_tp + det5_tn) / (det5_tp + det5_tn + det5_fp + det5_fn))
+            r1.append((det1_tp + det1_tn) /
+                      (det1_tp + det1_tn + det1_fp + det1_fn))
+            r2.append((det2_tp + det2_tn) /
+                      (det2_tp + det2_tn + det2_fp + det2_fn))
+            r3.append((det3_tp + det3_tn) /
+                      (det3_tp + det3_tn + det3_fp + det3_fn))
+            r4.append((det4_tp + det4_tn) /
+                      (det4_tp + det4_tn + det4_fp + det4_fn))
+            r5.append((det5_tp + det5_tn) /
+                      (det5_tp + det5_tn + det5_fp + det5_fn))
 
     plt.bar(np.array(downscale_factors) - 0.02, r1,
             color='blue', width=0.01, label="MTCNN")
@@ -214,8 +222,9 @@ def change_brightness_of_images(img_dir, brightness_factor, base_out_dir):
 
 
 def main():
-    global brightness_factors
-    global downscale_factors
+    brightness_factors = [0.1, 0.3, 0.6, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+    downscale_factors = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0]
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', "--input-dir", help="working directory (default: CWD)",
                         default=os.getcwd(), nargs='?')
@@ -227,16 +236,16 @@ def main():
                         default=os.getcwd() + "/out", nargs='?')
     parser.add_argument('-cb', "--change-brightness",
                         help="change brightness factor of images; factor > 1 to enhance brightness, factor < 1 to decrease", type=float)
-    parser.add_argument('-cbdf', "--change-brightness-default-factors",
-                        help="change brightness of images with default factors: " + str(brightness_factors), action='store_true')
+    parser.add_argument('-cbf', "--change-brightness-factors",
+                        help="change brightness of images with custom factors", nargs='*')
     parser.add_argument(
         '-d', "--downscale", help="downscale images by downscale factor; factor must be in range (0, 1>", type=float)
-    parser.add_argument('-ddf', "--downscale-default-factors",
-                        help="downscale with default factors: " + str(downscale_factors), action='store_true')
-    parser.add_argument('-beadf', "--brightness-effect-analysis-default-factors",
-                        help="try various face detection tools on lightened/dimmed face images and show results as a graph", action='store_true')
-    parser.add_argument('-deadf', "--downscale-effect-analysis-default-factors",
-                        help="try various face detection tools on downscalled face images and show results as a graph", action='store_true')
+    parser.add_argument('-df', "--downscale-factors",
+                        help="downscale images with custom factors", nargs='*')
+    parser.add_argument('-beaf', "--brightness-effect-analysis-factors",
+                        help="try various face detection tools on lightened/dimmed face images and show results as a graph", nargs='*')
+    parser.add_argument('-deaf', "--downscale-effect-analysis-factors",
+                        help="try various face detection tools on downscalled face images and show results as a graph", nargs='*')
     args = parser.parse_args()
 
     if args.filter_face_width:
@@ -254,18 +263,39 @@ def main():
         else:
             downscale_images(args.input_dir, args.downscale, args.output_dir)
             print("Images were saved to:", args.output_dir)
-    elif args.change_brightness_default_factors:
-        for f in brightness_factors:
-            change_brightness_of_images(args.input_dir, f, args.output_dir)
+    elif args.change_brightness_factors is not None:
+        if len(args.change_brightness_factors) == 0:
+            print("Using default brightness factors:", str(brightness_factors))
+            for f in brightness_factors:
+                change_brightness_of_images(args.input_dir, f, args.output_dir)
+        else:
+            for f in args.change_brightness_factors:
+                change_brightness_of_images(
+                    args.input_dir, float(f), args.output_dir)
         print("Images were saved to:", args.output_dir)
-    elif args.downscale_default_factors:
-        for f in downscale_factors:
-            downscale_images(args.input_dir, f, args.output_dir)
+    elif args.downscale_factors is not None:
+        if len(args.downscale_factors) == 0:
+            print("Using default brightness factors:", str(downscale_factors))
+            for f in downscale_factors:
+                downscale_images(args.input_dir, f, args.output_dir)
+        else:
+            for f in args.downscale_factors:
+                downscale_images(args.input_dir, float(f), args.output_dir)
         print("Images were saved to:", args.output_dir)
-    elif args.brightness_effect_analysis_default_factors:
-        do_analysis_brightness_effect(args.input_dir, brightness_factors)
-    elif args.downscale_effect_analysis_default_factors:
-        do_analysis_downscale_effect(args.input_dir, downscale_factors)
+    elif args.brightness_effect_analysis_factors is not None:
+        if len(args.brightness_effect_analysis_factors) == 0:
+            print("Using default brightness factors:", str(brightness_factors))
+            do_analysis_brightness_effect(args.input_dir, brightness_factors)
+        else:
+            do_analysis_brightness_effect(
+                args.input_dir, [float(i) for i in args.brightness_effect_analysis_factors])
+    elif args.downscale_effect_analysis_factors is not None:
+        if len(args.downscale_effect_analysis_factors) == 0:
+            print("Using default downscale factors:", str(downscale_factors))
+            do_analysis_downscale_effect(args.input_dir, downscale_factors)
+        else:
+            do_analysis_downscale_effect(
+                args.input_dir, [float(i) for i in args.downscale_effect_analysis_factors])
 
 
 if __name__ == "__main__":
